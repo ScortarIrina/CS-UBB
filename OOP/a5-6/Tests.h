4186ca013069a7a006b4f7fdc4869d10f9202fe1
916_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2022-04-27 22:17:38 +0300

    Create Tests.h

---
+++ OOP/a5-6/Tests.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "DynamicVector.h"
+#include "Tutorial.h"
+#include "Repository.h"
+#include "Service.h"
+
+class Tests
+{
+public:
+	void testDynamicVector();
+	void testTutorial();
+	void testRepository();
+	void testService();
+	void testWatchlist();
+};
