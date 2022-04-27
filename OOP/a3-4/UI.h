322476d0e086c7a86f6cd3a4fd3e4754188e596b
916_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2022-04-27 22:11:06 +0300

    Create UI.h

---
+++ OOP/a3-4/UI.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Controller.h"
+
+typedef struct
+{
+	Controller* controller;
+} UI;
+
+UI* createUI(Controller* controller);
+
+void destroyUI(UI* ui);
+
+void startUI(UI* ui);
