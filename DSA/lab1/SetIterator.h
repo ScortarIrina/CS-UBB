f0410385b27f9d0023ce8c37e1ef19ba9f4f3920
916_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2022-04-27 21:48:23 +0300

    Create SetIterator.h

---
+++ DSA/lab1/SetIterator.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Set.h"
+
+class SetIterator
+{
+	//DO NOT CHANGE THIS PART
+	friend class Set;
+private:
+	//DO NOT CHANGE THIS PART
+	const Set& set;
+	SetIterator(const Set& s);
+
+	int current_index;
+
+public:
+	void first();
+	void next();
+	TElem getCurrent();
+	bool valid() const;
+};
