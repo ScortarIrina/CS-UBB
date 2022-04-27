322003a6a9acf35f884aa12822f3c17d0eb6deeb
916_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2022-04-27 22:18:17 +0300

    Create Tutorial.h

---
+++ OOP/a5-6/Tutorial.h
@@ -0,0 +1,84 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<Windows.h>
+
+
+class Duration 
+{
+private:
+	int minutes;
+	int seconds;
+
+public:
+	// constructor
+	Duration();
+
+	Duration(int minutes, int seconds);
+
+	// copy constructor
+	Duration(const Duration&);
+
+	// destructor
+	~Duration();
+
+	// getters
+	int getMinutes();
+	int getSeconds();
+
+	// setters
+	void setMinutes(int minutes);
+	void setSeconds(int seconds);
+
+	// overloading '!=' operator
+	friend bool operator!=(const Duration& d1, const Duration& d2);
+
+	// overloading equality operator
+	friend bool operator==(const Duration& d1, const Duration& d2);
+
+	// overloading '<<' operator
+	// used when printing the duration
+	friend std::ostream& operator<<(std::ostream& os, const Duration& d);
+};
+
+class Tutorial
+{
+private:
+	std::string title;
+	std::string presenter;
+	std::string link;
+	Duration duration;
+	int nr_likes;
+
+public:
+	// constructor
+	Tutorial();
+
+	Tutorial(const std::string title, const std::string presenter, const Duration duration, const int nr_likes, const std::string link);
+
+	// copy constructor
+	Tutorial(const Tutorial&);
+
+	// getters
+	std::string getTitle() const;
+	std::string getPresenter() const;
+	std::string getLink() const;
+	Duration getDuration() const;
+	int getLikes() const;
+
+	// setters
+	void setTitle(std::string title);
+	void setPresenter(std::string presenter);
+	void setDuration(int minutes, int seconds);
+	void setLikes(int nr_likes);
+	void setLink(std::string link);
+
+	// overloading equality operator
+	friend bool operator==(const Tutorial& t1, const Tutorial& t2);
+
+	// overloading '<<' operator
+	// used when printing the tutorials
+	friend std::ostream& operator<<(std::ostream& os, const Tutorial& t);	
+
+	void play() const;
+};
