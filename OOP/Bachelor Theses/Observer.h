5cec7b7c7e435d2ee99c5edbd6332efd711a9ecf
916_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2022-07-01 12:20:48 +0300

    Create Observer.h

---
+++ OOP/Bachelor Theses/Observer.h
@@ -0,0 +1,13 @@
+//
+// Created by Irina Scortar on 01.07.2022.
+//
+
+#ifndef BACHELOR_THESES_2_OBSERVER_H
+#define BACHELOR_THESES_2_OBSERVER_H
+
+class Observer {
+public:
+    virtual void update() = 0;
+};
+
+#endif //BACHELOR_THESES_2_OBSERVER_H
