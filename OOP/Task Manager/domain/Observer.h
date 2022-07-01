b8581703f518cddd5bd9f9866ecec212ec7f8231
916_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2022-07-01 16:26:02 +0300

    Create Observer.h

---
+++ OOP/Task Manager/domain/Observer.h
@@ -0,0 +1,13 @@
+//
+// Created by Irina Scortar on 01.07.2022.
+//
+
+#ifndef TASK_MANAGER_2_OBSERVER_H
+#define TASK_MANAGER_2_OBSERVER_H
+
+class Observer {
+public:
+    virtual void update() = 0;
+};
+
+#endif //TASK_MANAGER_2_OBSERVER_H
