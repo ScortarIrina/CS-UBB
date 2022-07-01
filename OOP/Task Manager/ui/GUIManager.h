6f8422069ed8bebe8026ddf43d704d8a19a41de1
916_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2022-07-01 16:33:33 +0300

    Create GUIManager.h

---
+++ OOP/Task Manager/ui/GUIManager.h
@@ -0,0 +1,17 @@
+//
+// Created by Irina Scortar on 01.07.2022.
+//
+
+#ifndef TASK_MANAGER_2_GUIMANAGER_H
+#define TASK_MANAGER_2_GUIMANAGER_H
+
+#include "controller/Controller.h"
+
+
+class GUIManager {
+public:
+    GUIManager(Controller &controller);
+};
+
+
+#endif //TASK_MANAGER_2_GUIMANAGER_H
