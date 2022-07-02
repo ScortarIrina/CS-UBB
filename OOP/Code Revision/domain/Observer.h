1ef8045b6316e6a8c92b3733ed51e1064e2b1dc5
916_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2022-07-02 18:43:25 +0300

    Create Observer.h

---
+++ OOP/Code Revision/domain/Observer.h
@@ -0,0 +1,13 @@
+//
+// Created by Irina Scortar on 01.07.2022.
+//
+
+#ifndef CODE_REVISION_OBSERVER_H
+#define CODE_REVISION_OBSERVER_H
+
+class Observer {
+public:
+    virtual void update() = 0;
+};
+
+#endif //CODE_REVISION_OBSERVER_H
