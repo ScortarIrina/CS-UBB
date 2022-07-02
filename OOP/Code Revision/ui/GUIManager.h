e4db07ce2c8046526bd8f8efad0ae5446eb77125
916_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2022-07-02 18:47:51 +0300

    Create GUIManager.h

---
+++ OOP/Code Revision/ui/GUIManager.h
@@ -0,0 +1,17 @@
+//
+// Created by Irina Scortar on 02.07.2022.
+//
+
+#ifndef CODE_REVISION_GUIMANAGER_H
+#define CODE_REVISION_GUIMANAGER_H
+
+#include "controller/Controller.h"
+
+
+class GUIManager {
+public:
+    explicit GUIManager(Controller &controller);
+};
+
+
+#endif //CODE_REVISION_GUIMANAGER_H
