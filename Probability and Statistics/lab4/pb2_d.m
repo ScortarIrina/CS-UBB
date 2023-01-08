831bc16f60e80d2b79024f773605b6e94fd46468
926_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2023-01-08 20:16:44 +0200

    Create pb2_d.m

---
+++ Probability and Statistics/lab4/pb2_d.m
@@ -0,0 +1,22 @@
+clear
+n = input('n = ');   % number of trials in bino
+p = input('p = ');   % probability
+N = input('N = ');   % number of simulations
+
+for i = 1 : N
+    for j = 1 : n
+        X(j) = 0;
+        while rand >= p
+            X(j) = X(j) + 1;
+        endwhile
+    endfor
+    Y(i) = sum(X);
+endfor
+
+K = 0 : 150;
+U_Y = unique(Y);
+n_Y = hist(Y, length(U_Y));
+rel_freq = n_Y/N;
+
+plot(U_Y, rel_freq, "b*", K, nbinpdf(K, n, p), "r*")
+legend("neg.bino.", "geopdf")
