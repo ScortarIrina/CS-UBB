00018cc556923da218fca091241170c83a9cc9b2
926_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2023-01-09 11:41:42 +0200

    Update pb2.m

--- Probability and Statistics/lab3/pb2.m
+++ Probability and Statistics/lab3/pb2.m
@@ -1,3 +1,12 @@
+% 1. Let X have one of the following distributions: X ∈ N(μ,σ) (normal), X ∈ T(n) (Student), 
+% X ∈ χ2(n), or X ∈ F(m,n) (Fischer). Compute the following:
+% a) P(X ≤ 0) and P(X ≥ 0); 
+% b)P(−1≤X≤1)andP(X≤−1or X≥1);
+% c) the value xα such that P(X < xα) = α, for α ∈ (0,1) (xα is called the quan- tile of order α);
+% d) the value xβ such that P(X > xβ) = β, for β ∈ (0,1) (xβ is the quantile of order 1 − β).
+
+% ~~~ STUDENT DISTRIBUTION ~~~
+
 n = input("Please input n: ");
 p = input("Please input p: ");
 X = 0:1:n;
