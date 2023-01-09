c0a3331cd18722ae5b4846d56a5c60dee7fd4422
926_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2023-01-09 11:06:08 +0200

    Update pb1.m

--- Probability and Statistics/lab1/pb1.m
+++ Probability and Statistics/lab1/pb1.m
@@ -1,5 +1,5 @@
 % For the matrices 
-%               [1  0 -2] 		        [2  1  1]
+%                 [1  0 -2] 		        [2  1  1]
 %	          A = [2  1  3]		      B = [1  0 -1]
 %	              [0  1  0]		          [1  1  0]
 % print the matrices C = A−B, D = A·B and E = [eij], where eij =aij ·bij.
