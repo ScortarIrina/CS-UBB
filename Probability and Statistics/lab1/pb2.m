4ab60b0a1a98618cc5387de707837d97fdc5bb0a
926_Scorțar_Irina <91870013+ScortarIrina@users.noreply.github.com>
2023-01-09 11:06:48 +0200

    Update pb2.m

--- Probability and Statistics/lab1/pb2.m
+++ Probability and Statistics/lab1/pb2.m
@@ -1,4 +1,4 @@
-% For x ∈ [0, 3], graph on the same set of axes the functions x5/10, x*sin x and cos x,
+% For x ∈ [0, 3], graph on the same set of axes the functions x^5/10, x*sin x and cos x,
 % in different colours and line-styles. Display a title and a legend on your graph. Then plot them % on different pictures.
 
 x=-1:0.1:1
